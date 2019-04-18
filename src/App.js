eb263eb3426a85bb491d4465161a34db40b36800
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-18 13:19:35 +0700

    set up start project and install darft-js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React with Draft.js</h1>
+            </div>
+        )
+    }
+}
+
+export default App
