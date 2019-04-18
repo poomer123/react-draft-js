f87e96e128528b7dd497cab6c0ff12089fc5c896
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-18 13:53:03 +0700

    basic editor without style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { Editor, EditorState } from 'draft-js'
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>React with Draft.js</h1>
-            </div>
-        )
+const App = (props) => {
+    const [editorState, setEditorState] = useState( EditorState.createEmpty() )
+    const editor = useRef(null)
+    
+    const style = {
+        editor: {
+            border: '1px solid gray',
+            minHeight: '150px',
+            width: '400px'
+        }
     }
+
+    function focusEditor() {
+        editor.current.focus()
+    }
+    
+    useEffect(() => {
+        focusEditor()
+    }, [])
+
+    return (
+        <div>
+            <h1>React with Draft.js</h1>
+            <div style={style.editor} onClick={focusEditor}>
+                <Editor
+                    ref={editor}
+                    editorState={editorState}
+                    onChange={editorState => setEditorState(editorState)}
+                />
+            </div>
+        </div>
+    )
+    
 }
 
 export default App
